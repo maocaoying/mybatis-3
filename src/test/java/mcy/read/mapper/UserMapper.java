d1f26a19f2e8cd01c5211b88c05b05464828491d
yinxiaokuan <vic.yin@b-and-qchina.com>
2018-09-28 19:20:53 +0800

    Test

---
+++ src/test/java/mcy/read/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package mcy.read.mapper;
+
+import mcy.read.po.User;
+
+/**
+ * @author : Vic.Yin
+ * @version v.0.1
+ * @date : 2018/09/28
+ * @Description:
+ */
+public interface UserMapper {
+    User getUser(Integer id);
+}
